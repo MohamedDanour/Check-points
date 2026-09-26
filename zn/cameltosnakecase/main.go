7e2f8d3eae6401f859e74f892e7e0ee9e7225cf7
zinebnadak <zineb@nadak.ai>
2026-09-26 12:46:52 +0300

    docs: collaborator folder by zinebnadak

---
+++ zn/cameltosnakecase/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(CamelToSnakeCase("HelloWorld"))
+	fmt.Println(CamelToSnakeCase("helloWorld"))
+	fmt.Println(CamelToSnakeCase("camelCase"))
+	fmt.Println(CamelToSnakeCase("CAMELtoSnackCASE"))
+	fmt.Println(CamelToSnakeCase("camelToSnakeCase"))
+	fmt.Println(CamelToSnakeCase("hey2"))
+}
+
+func CamelToSnakeCase(s string) string{
+
+}
